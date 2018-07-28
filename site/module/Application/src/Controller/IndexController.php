6875882e1dc473a7e97481b7175784091025da3e
Rogelso <rogelso.salvi@unochapeco.edu.br>
2018-07-27 21:40:47 -0300

    commit

--- site/module/Application/src/Controller/IndexController.php
+++ site/module/Application/src/Controller/IndexController.php
@@ -14,7 +14,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';d
+require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
 class IndexController extends AbstractActionController
